7ae2bf8054c00153a8c5ea99b2a10d0cc572cf6c
Dario Di Girolamo <d.digirolamo25@gmail.com>
2021-05-13 15:28:28 -0400

    forth commit - caluclated equilibrium points, regime shift and plotted them. Parameter and values should be looked into since the regime shift is observed at time step 0

--- Simulation_4_DD.R
+++ Simulation_4_DD.R
@@ -86,6 +86,8 @@ plot(time_series_q_4, type="l")
 
 library(dplyr)
 
+#set up fisher informaiton calculations for the simulation
+
 n_step_4 = length(Time_plot_4)
 
 calc_1st_deriv_4 = function(y,delta_4) (lead(y,1) - lag(y,1))/(2*delta_4)
@@ -121,31 +123,33 @@ plot(sim4[,"time"],
      ylab="Fisher Information - Rolling Mean",
      xlab="time", log='y')
 
+#set up jacobian matrix
+
 troph_tri_jacobian_4 <- function(t,y,parms){
   
   jacobian_4 <- matrix(NA, nrow=3, ncol=3)
   
-  #dP - differentiation variable: A  
-  jacobian_4[1,1] <- with(parms, 
-  #dP - differentiation variable: F 
+  #dA - differentiation variable: A -q(t) - m_a 
+  jacobian_4[1,1] <- with(parms, - q(t) - m_A)
+  #dA - differentiation variable: F 
   jacobian_4[1,2] <- (0)
-  #dP - differentiation variable: J 
-  jacobian_4[1,3] <- with(parms, 
+  #dA - differentiation variable: J 
+  jacobian_4[1,3] <- with(parms, s)
   
   
   #dF - differentiation variable: A 
-  jacobian_4[2,1] <- with(parms, 
+  jacobian_4[2,1] <- with(parms, -c_FA*y[2])
   #dF - differentiation variable: F 
-  jacobian_4[2,2] <- with(parms, 
+  jacobian_4[2,2] <- with(parms, -D_F - y[1]*c_FA)
   #dF - differentiation variable: J 
   jacobian_4[2,3] <- (0)
   
   #dJ - differentiation variable: A 
-  jacobian_4[3,1] <- with(parms, 
+  jacobian_4[3,1] <- with(parms, y[2] - c_JA*y[3])
   #dJ - differentiation variable: F
-  jacobian_4[3,2] <- with(parms, 
+  jacobian_4[3,2] <- with(parms, -((c_JF*y[3]*v)/(c_JF*y[2]+v+h))+((((c_JF)^2)*y[3]*v*y[2])/(((c_JF*y[2])+v+h)^2))+y[1])
   #dJ - differentiation variable: J
-  jacobian_4[3,3] <- with(parms, 
+  jacobian_4[3,3] <- with(parms, -((c_JF*y[2]*v)/(v+h+c_JF*y[2]))-s-(y[1]*c_JA))
   
   
   
@@ -162,84 +166,84 @@ troph_tri_jacobian_4 <- function(t,y,parms){
 #eigen_jacobian_1$vectors
 
 #create a vector of 600 time steps
-times <- seq(0, 600, by=1)
-n_steps <- length(times)
+times_4 <- seq(0, 1020, by=1)
+n_steps_4 <- length(times_4)
 
 #create a data frame to hold equilibrium values
-stable_states_1 <- data.frame(P = rep(0,times = n_steps),
-                              F = rep(0,times = n_steps),
-                              J = rep(0,times = n_steps),
-                              eigen  = rep(0,times = n_steps),
-                              time = times)
+stable_states_4 <- data.frame(A = rep(0,times_4 = n_steps_4),
+                              F = rep(0,times_4 = n_steps_4),
+                              J = rep(0,times_4 = n_steps_4),
+                              eigen  = rep(0,times_4 = n_steps_4),
+                              time = times_4)
 
 #set the current state for the loop to the original initial condition
-current_state_1 <- init_cond
+current_state_4 <- init_cond_4
 
-for(i in 1:n_steps){
-  current_time <- times[i]
+for(i in 1:n_steps_4){
+  current_time_4 <- times_4[i]
   #calculate the closest equilibrium point at the current time step
-  root_value_1 <- stode(y= current_state_1,
-                        time =current_time,
-                        func = troph_tri_static_1,
-                        jacfunc = troph_tri_jacobian_1,
-                        parms = model_parameters_1,
+  root_value_4 <- stode(y= current_state_4,
+                        time =current_time_4,
+                        func = trophic_system_4,
+                        jacfunc = troph_tri_jacobian_4,
+                        parms = model_parameters_4,
                         positive = TRUE #this ensures that rootSolve will only find positive (or zero) solutions
   )
   
   #change the current state to this value
-  current_state_1 <- root_value_1$y
-  stable_states_1[i, c("P","F","J")] <- current_state_1
+  current_state_4 <- root_value_4$y
+  stable_states_4[i, c("A","F","J")] <- current_state_4
   
   #Calculate the Jacobian of the system at this equilibrium
-  current_jacobian_1 <- troph_tri_jacobian_1(t = current_time, 
-                                             y = current_state_1,
-                                             parms = model_parameters_1)
+  current_jacobian_4 <- troph_tri_jacobian_4(t = current_time_4, 
+                                             y = current_state_4,
+                                             parms = model_parameters_4)
   
   #calculate eigenvalues of this Jacobian and find the maximum real eigenvalue
-  current_eigs_1 <- eigen(current_jacobian_1)
-  stable_states_1[i,"eigen"] <- max(Re(current_eigs_1$values))
+  current_eigs_4 <- eigen(current_jacobian_4)
+  stable_states_4[i,"eigen"] <- max(Re(current_eigs_4$values))
   
   #add a small perturbation to the current state to keep rootSolve from finding
   #only zero values after the regime shift.
-  current_state_1 <- current_state_1 +0.5
+  current_state_4 <- current_state_4 +0.5
 }
 
 #Find the regime shift point as the place where the eigen value of the Jacobian goes to zero (or just above)
-regime_shift_1 <- stable_states_1$time[stable_states_1$eigen==max(stable_states_1$eigen)]
+regime_shift_4 <- stable_states_4$time[stable_states_4$eigen==max(stable_states_4$eigen)]
 
 #time step of regime shift
-regime_shift_1
+regime_shift_4
 
 #Plot all three time series, and the eigenvalues and Fisher information indices;
 #mfcol specifies to add plots column-wise (fillin)
 par(mfcol =c(3,2))
 
 #plot the simulation, stable state, and annotate the regime shift for predators
-plot(P~time, data = sim1, type="l")
-points(P~time, data= stable_states_1,type="l",col ="red")
-abline(v = regime_shift_1, col ="blue", lty=2)
+plot(A~time, data = sim4, type="l")
+points(A~time, data= stable_states_4,type="l",col ="red")
+abline(v = regime_shift_4, col ="blue", lty=2)
 
 #plot the simulation, stable state, and annotate the regime shift for forage fish
-plot(F~time, data= sim1,type="l")
-points(F~time, data= stable_states_1,type="l",col ="red")
-abline(v = regime_shift_1, col ="blue", lty=2)
+plot(F~time, data= sim4,type="l")
+points(F~time, data= stable_states_4,type="l",col ="red")
+abline(v = regime_shift_4, col ="blue", lty=2)
 
 #plot the simulation, stable state, and annotate the regime shift for juveniles
-plot(J~time, data= sim1,type="l")
-points(J~time, data= stable_states_1,type="l",col ="red")
-abline(v = regime_shift_1, col ="blue", lty=2)
+plot(J~time, data= sim4,type="l")
+points(J~time, data= stable_states_4,type="l",col ="red")
+abline(v = regime_shift_4, col ="blue", lty=2)
 
 #plotting eigenvalues
-plot(eigen~time, data= stable_states_1,type="l")
-abline(v = regime_shift_1, col ="blue", lty=2)
+plot(eigen~time, data= stable_states_4,type="l")
+abline(v = regime_shift_4, col ="blue", lty=2)
 
 #plotting Fisher Information
-plot(sim1[,"time"],
-     rolling_mean_fisher_1,
+plot(sim4[,"time"],
+     rolling_mean_fisher_4,
      type="l",
      ylab="Fisher Information - Rolling Mean",
      xlab="time", log='y')
-abline(v = regime_shift_1, col ="blue", lty=2)
+abline(v = regime_shift_4, col ="blue", lty=2)
 
 
 
