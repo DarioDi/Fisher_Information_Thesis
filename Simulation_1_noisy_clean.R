0020ce7731d87f0dedb1d06f69f9cdd29d9590da
Valentin <valentin.lucet@gmail.com>
2021-06-14 15:01:40 -0400

    change parameters

--- Simulation_1_noisy_clean.R
+++ Simulation_1_noisy_clean.R
@@ -8,7 +8,7 @@ library(tibbletime)
 source("helper_functions.R")
 
 sim1_parameter = expand.grid(obs_error = seq(0.01, 1, length.out = 5),
-                             rate_of_change = seq(0.0001, 0.001, length.out = 5))
+                             rate_of_change = seq(0.0001, 0.01, length.out = 10))
 sim1_parameter$replicate <- 1:nrow(sim1_parameter)
 
 n_sim1 = nrow(sim1_parameter)
